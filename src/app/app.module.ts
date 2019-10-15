2033303d0ed5b5cd7149075b499b3296266bc35d
Martin Smolka <martinsmolka@martins-mbp-2.wireless-1x.unc.edu>
2019-10-15 16:13:56 -0400

    changes to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { AceEditorModule } from 'ng2-ace-editor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,6 +17,10 @@ import { LandingComponent } from './components/landing/landing.component';
     BrowserModule,
     AppRoutingModule,
     AceEditorModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
